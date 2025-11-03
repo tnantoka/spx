21d0be36a1e0d9736e4c0145d2beb746dccefad1
tnantoka <tnantoka@bornneet.com>
2025-11-03 22:46:42 +0900

    feat: bundle gem spx --bin --git --github-username=tnantoka --mit --test=rspec --ci=github --linter=rubocop

---
+++ lib/spx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spx/version"
+
+module Spx
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/spx_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Spx do
+  it "has a version number" do
+    expect(Spx::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
